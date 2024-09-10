4e70f05bbb26a5b55d6cae8593ae238ed2daa85c
CETECOM <CETECOM@AVAWSV02LC1005.duoc.acad>
2024-09-10 15:38:46 -0300

    src completo

--- app/src/app/new-pass/new-pass.page.ts
+++ app/src/app/new-pass/new-pass.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-new-pass',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPassPage implements OnInit {
 
-  constructor() { }
+  constructor(private nvt: NavController) {}
+
+  navlog() {
+    this.nvt.navigateForward(['/log']);
+  }
+ 
+  navregister() {
+    this.nvt.navigateForward(['/register']);
+  }
 
   ngOnInit() {
   }
